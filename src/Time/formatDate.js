d0f409c0c52ae21106817236b49d034c6322b267
csh <308241863@qq.com>
2018-03-01 17:05:53 +0800

    update formatDate

--- src/Time/formatDate.js
+++ src/Time/formatDate.js
@@ -6,49 +6,50 @@
  * @returns 格式化后日期 例:2018-02-24 18:18:18
  */
 const formatDate = (time = new Date(), format = 'YY-MM-DD hh:mm:ss') => {
-  const reg_y = /YY(\S|\s)*MM/,
-    reg_mo = /MM(\S|\s)*DD/,
-    reg_d = /DD(\S|\s)*hh/,
-    reg_h = /hh(\S|\s)*mm/,
-    reg_m = /mm(\S|\s)*ss/,
-    reg_s = /ss(\S|\s)*/;
-  const dateList = {};
-  //年
-  if (format.match(reg_y)) {
-    let y = time.getFullYear(),
-      f = format.match(reg_y)[1] || '';
-    dateList.year = y + f;
-  }
-  //月
-  if (format.match(reg_mo)) {
-    let m = time.getMonth(),
-      f = format.match(reg_mo)[1] || '';
-    dateList.month = m + 1 < 10 ? '0' + (m + 1) + f : m + 1 + f;
-  }
-  //日
-  if (format.match(reg_d)) {
-    let d = time.getDate(),
-      f = format.match(reg_d)[1] || '';
-    dateList.day = d < 10 ? '0' + d + f : d + f;
-  }
-  //时
-  if (format.match(reg_h)) {
-    let h = time.getHours(),
-      f = format.match(reg_h)[1] || '';
-    dateList.hour = h < 10 ? '0' + h + f : h + f;
-  }
-  //分
-  if (format.match(reg_m)) {
-    let m = time.getMinutes(),
-      f = format.match(reg_m)[1] || '';
-    dateList.min = m < 10 ? '0' + m + f : m + f;
-  }
-  //秒
-  if (format.match(reg_s)) {
-    let s = time.getSeconds(),
-      f = format.match(reg_s)[1] || '';
-    dateList.sec = s < 10 ? '0' + s + f : s + f;
-  }
-  return Object.values(dateList).reduce((a, b) => a + b, '');
+  const v_list = {
+    //年
+    YY: time.getFullYear(),
+    //月
+    MM:
+      time.getMonth() + 1 < 10
+        ? '0' + (time.getMonth() + 1)
+        : time.getMonth() + 1,
+    //日
+    DD: time.getDate() < 10 ? '0' + time.getDate() : time.getDate(),
+    //时
+    hh: time.getHours() < 10 ? '0' + time.getHours() : time.getHours(),
+    //分
+    mm: time.getMinutes() < 10 ? '0' + time.getMinutes() : time.getMinutes(),
+    //秒
+    ss: time.getSeconds() < 10 ? '0' + time.getSeconds() : time.getSeconds()
+  };
+  return [
+    {
+      id: format.indexOf('YY'),
+      v: v_list.YY + format.charAt(format.indexOf('YY') + 2)
+    },
+    {
+      id: format.indexOf('MM'),
+      v: v_list.MM + format.charAt(format.indexOf('MM') + 2)
+    },
+    {
+      id: format.indexOf('DD'),
+      v: v_list.DD + format.charAt(format.indexOf('DD') + 2)
+    },
+    {
+      id: format.indexOf('hh'),
+      v: v_list.hh + format.charAt(format.indexOf('hh') + 2)
+    },
+    {
+      id: format.indexOf('mm'),
+      v: v_list.mm + format.charAt(format.indexOf('mm') + 2)
+    },
+    {
+      id: format.indexOf('ss'),
+      v: v_list.ss + format.charAt(format.indexOf('ss') + 2)
+    }
+  ]
+    .filter(v => v.id > -1)
+    .reduce((a, b) => a + b.v, '');
 };
 module.exports = formatDate;
